267fe17e4f06ea464250674e14d0177d8eb29810
Saul Roldan <saulry90@gmail.com>
2025-10-14 22:17:42 +0200

    Add header responsive and styles fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
 import '../css/style.css';
 
-console.log("✅ Portfolio iniciado correctamente");
+// ===== MENÚ MÓVIL =====
+document.addEventListener('DOMContentLoaded', () => {
+  const navToggle = document.querySelector('.nav__toggle');
+  const nav = document.querySelector('.nav');
+
+  navToggle.addEventListener('click', () => {
+    nav.classList.toggle('nav--visible');
+  });
+});

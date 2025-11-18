5a9652e10b0b552d80ff772f73022f0e4a7d513c
Saul Roldan <saulry90@gmail.com>
2025-11-18 21:53:58 +0100

    General fixes, header and hero refactor, add toggle to dark mode

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,50 @@
-// ===== MENÚ MÓVIL =====
-document.addEventListener('DOMContentLoaded', () => {
-  const navToggle = document.querySelector('.nav__toggle');
-  const nav = document.querySelector('.nav');
+  // ===== ICONOS FONT AWESOME =====
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { faMoon, faSun, faBars } from '@fortawesome/free-solid-svg-icons';
+library.add(faMoon, faSun, faBars);
+dom.watch();
 
-  navToggle.addEventListener('click', () => {
-    nav.classList.toggle('nav--visible');
-  });
+document.addEventListener('DOMContentLoaded', () => {
+  // ===== MENU MOBILE =====
+  const menuToggle = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('.nav');
+    const MENU_VISIBLE_CLASS = 'nav--visible';
+
+    if (menuToggle && nav) {
+        menuToggle.addEventListener('click', () => {
+            nav.classList.toggle(MENU_VISIBLE_CLASS);
+            
+            const isExpanded = nav.classList.contains(MENU_VISIBLE_CLASS);
+            menuToggle.setAttribute('aria-expanded', isExpanded);
+        });
+    }
+
+  // ===== TOGGLE DEL MODO OSCURO =====
+    const toggleButton = document.getElementById('theme-toggle');
+    const htmlElement = document.documentElement;
+
+    function toggleSwitchState() {
+        htmlElement.classList.toggle('is-dark');
+    }
+
+    toggleButton.addEventListener('click', toggleSwitchState);
+
+  // ===== SOMBRA EN EL HEADER AL HACER SCROLL =====
+  const header = document.querySelector('.header');
+    
+    if (!header) return;
+
+    const SCROLL_THRESHOLD = 150;
+
+    function toggleHeaderShadow() {
+        if (window.scrollY > SCROLL_THRESHOLD) {
+            header.classList.add('header--scrolled');
+        } else {
+            header.classList.remove('header--scrolled');
+        }
+    }
+
+    toggleHeaderShadow(); 
+    
+    window.addEventListener('scroll', toggleHeaderShadow);
 });
